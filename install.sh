f3d4f937afad3d1a0090d657672fc00e398687dd
Max Heiber <max.heiber@gmail.com>
2017-02-21 19:00:04 -0500

    sublime settings

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 cd ~/dotfiles
 
+# link dot files
 for file in $(ls -a | grep "^\."); do
   if [ "$file" != "." ] && [ "$file" != ".." ] && [ "$file" != ".git" ]; then
     cmd="ln -s $(pwd)/$file ~/$file"
@@ -8,4 +9,6 @@ for file in $(ls -a | grep "^\."); do
   fi
 done
 
+# Sublime settings
+ln -s ~/dotfiles/subl-setttings/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
 
