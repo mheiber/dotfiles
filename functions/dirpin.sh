a8d4f192d3b46f1378d3b2f3c7344d3dc18abfce
Max Heiber <max.heiber@gmail.com>
2017-12-28 15:14:18 +0800

    pin directories

---
+++ functions/dirpin.sh
@@ -0,0 +1,11 @@
+# Usage:
+# `dirpin` pins current directory so can just type `<short_dir_name>` instead of `cd <long_dir_name>` to go
+# `dirpin <alias>` does the same, but uses alias instead of `short_dir_name`
+
+cmd="cd $(pwd)"
+short_dir=$(pwd | rev | cut -d"/" -f 1 | rev)
+func_name=${1:-$short_dir}
+echo $func_name
+# echo $cmd > $FUNCTIONS_DIR/$short_dir.sh
+fsrc
+
